e722ef8f04c3676c3990f6b84c155e6e65092c94
Nitesh Seram <niteshseram@gmail.com>
2025-01-22 10:56:43 +0530

    chore: update libraries

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,10 +1,9 @@
-import { ImageResponse } from '@vercel/og'
+import { ImageResponse } from 'next/og'
 import { NextRequest } from 'next/server'
 
 export const config = {
 	runtime: 'edge',
 }
-
 
 export default async function handler(req: NextRequest) {
 	const { searchParams } = req.nextUrl
